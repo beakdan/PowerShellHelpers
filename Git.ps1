132af350084653a474a212b063ea6d5274fa8b68
Adan Bucio <beakdan@gmail.com>
2018-03-26 12:18:41 -0600

    Added git helper functions

---
+++ Git.ps1
@@ -0,0 +1,143 @@
+﻿#Author: Adán Bucio
+#Dependencies: Don't forget to install git first
+
+$gitExe = "$env:ProgramFiles\Git\bin\git.exe";
+
+function Find-GitCurrentBranch {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)]
+            [string]$RepoPath
+	)
+
+    $gitParams = @('-C', """$($RepoPath.TrimEnd("\"))""", 'symbolic-ref', '--short', 'HEAD');
+    return [string](& $gitExe $gitParams);
+}
+
+#Git Checkout
+function Request-GitBranch {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)]
+            [string]$RepoPath,
+        [Parameter(Mandatory=$true)]
+            [string]$Branch
+	)
+
+    if(-not [System.IO.Directory]::Exists($RepoPath)) {
+        throw New-Object System.IO.DirectoryNotFoundException -ArgumentList $RepoPath
+    }
+
+    $gitParams = @('-C', """$($RepoPath.TrimEnd("\"))""", '-c', 'diff.mnemonicprefix=false', '-c', 'core.quotepath=false');
+
+    $status = "Already in branch '$Branch'";
+
+    if($Branch -ne (Find-GitCurrentBranch -RepoPath $RepoPath)){
+        $status = [string](
+            & $gitExe $gitParams checkout $Branch 2>&1 | % {
+                if($_ -is [System.Management.Automation.ErrorRecord]) {
+                    #Ps sees Branch checkout as an exception
+                    $_.Exception.Message
+                }
+            });
+        & $gitExe $gitParams submodule update --init --recursive *>$null
+    }
+
+    return $status;
+}
+
+#Git Fetch
+function Sync-GitLocalRepository {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)]
+            [string]$RepoPath
+	)
+
+    $gitParams = @('-C', """$($RepoPath.TrimEnd("\"))""", '-c', 'diff.mnemonicprefix=false', '-c', 'core.quotepath=false');
+
+    & $gitExe $gitParams fetch origin *>$null;
+    $StatusMsg = [string](& $gitExe $gitParams status 2>$null);
+    $BranchStatusMatch = [regex]::Match($StatusMsg, '(?:branch is )(?<status>[^\s]+) ', 
+            [System.Text.RegularExpressions.RegexOptions]::IgnoreCase -bor 
+            [System.Text.RegularExpressions.RegexOptions]::Multiline);
+
+    return $BranchStatusMatch.Groups[1].Value;
+}
+
+#Git Pull
+function Merge-GitWorkspaceFromRemote {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)]
+            [string]$RepoPath,
+        [Parameter(Mandatory=$true, ParameterSetName='Current')]
+            [switch]$UseCurrentBranch,
+        [Parameter(Mandatory=$true, ParameterSetName='Specify')]
+            [string]$Branch
+	)
+
+    if($UseCurrent.IsPresent) {
+        $Branch = (Find-GitCurrentBranch -RepoPath $RepoPath);
+    }
+
+    $gitParams = @('-C', """$($RepoPath.TrimEnd("\"))""", '-c', 'diff.mnemonicprefix=false',
+        '-c', 'core.quotepath=false', 'pull', 'origin', $Branch);
+
+    return [string](& $gitExe $gitParams)
+}
+
+function Get-GitFileLastCommit {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)]
+            [string]$RepoPath,
+        [Parameter(Mandatory=$true)]
+            [string]$FileRelativePath
+	)
+
+    $gitParams = @('-C', """$($RepoPath.TrimEnd("\"))""", 'log', '-1', '--format="%aI"', '--', """$FileRelativePath""");
+
+    $file = [System.IO.Path]::Combine($RepoPath, $FileRelativePath);
+    if(-not [System.IO.File]::Exists($file)) {
+        throw New-Object System.IO.FileNotFoundException `
+            -ArgumentList ([System.IO.Path]::GetFileName($file));
+    }
+
+    $gitOutput = [string](& $gitExe $gitParams);
+    $result = if([string]::IsNullOrWhiteSpace($gitOutput)) { [datetime]::MinValue } else {
+                    [System.DateTimeOffset]::ParseExact($gitOutput, 'yyyy-MM-ddTHH:mm:sszzz',
+                        [cultureinfo]::InvariantCulture
+                    ).UtcDateTime
+                };
+    return $result
+}
+
+<#
+Functions defined with standard verbs but, it's easier
+to use the functions aliased with Git* prefix
+#>
+Set-Alias -Name GitCurrentBranch -Value Find-GitCurrentBranch
+Set-Alias -Name GitCheckout -Value Request-GitBranch
+Set-Alias -Name GitFetch -Value Sync-GitLocalRepository
+Set-Alias -Name GitPull -Value Merge-GitWorkspaceFromRemote
+Set-Alias -Name GitFileLastCommit -Value Get-GitFileLastCommit
+
+#Export-ModuleMember -Function Find-GitCurrentBranch, Request-GitBranch, Sync-GitLocalRepository,
+#                        Merge-GitWorkspaceFromRemote, Get-GitFileLastCommit `
+#                    -Alias GitCurrentBranch, GitCheckout, GitFetch, GitPull, GitFileLastCommit;
+
+<#
+#Usage:
+
+GitCurrentBranch -RepoPath 'E:\Repositories\MyRepo'
+
+GitCheckout -RepoPath 'E:\Repositories\MyRepo' -Branch master
+
+GitFetch -RepoPath 'E:\Repositories\MyRepo'
+
+GitPull -RepoPath 'E:\Repositories\MyRepo' -UseCurrentBranch
+GitPull -RepoPath 'E:\Repositories\MyRepo' -Branch myBranch
+
+GitFileLastCommit -RepoPath 'E:\Repositories\MyRepo' -FileRelativePath 'folder\myfile.xxx'
+#>
--- README.md
+++ README.md
@@ -1,6 +1,25 @@
 # PowerShellHelpers
 A simple attempt to put order to the collection of scripts that I use on a daily basis.
 
-## Export-ExcelXLSX
+## Export-ExcelXLSX.ps1
 I really like how simple is to create Worksheets usign [ClosedXML](https://github.com/ClosedXML/ClosedXML) assembly. So I wrote this little function to produce xlsx files from the pipeline.
 ClosedXML can add a DataTable as a Worksheet and I heavily use that feature to dump complete resultsets, so this function also accepts an array of DataTables.
+
+## Git.ps1
+Some helper functions for common task executed on git repositories. Although function names were defined to honor the aproved PS verb list, I defined alias that are more clear.
+
+* Find-GitCurrentBranch (Alias: *GitCurrentBranch*)
+: Returns the name of the branch currently checked out
+
+* Request-GitBranch (Alias: *GitCheckout*)
+: Checks out the specified branch if it's not already checked out and returns an informative message
+
+* Sync-GitLocalRepository (Alias: *GitFetch*)
+: Executes a git Fetch and returns an informative message (*behind*, *up-to-date or *ahead*)
+
+* Merge-GitWorkspaceFromRemote (Alias: *GitPull*)
+: Executes a Pull and returns the command output
+
+* Get-GitFileLastCommit (Alias: *GitFileLastCommit*)
+: Gets the last commit date from a file at UTC 0
+
